dab82f82322a29c8dafb782ab8f52961f6bff872
Marc-Antoine Fortier <marc-antoine.fortier@fikaproductions.com>
2023-07-10 14:07:39 -0400

    feat: add value mapping signatures to NetworkDictionary extensions

--- com.unity.netcode.gameobjects/Runtime/Extensions/NetworkDictionaryExtensions.cs
+++ com.unity.netcode.gameobjects/Runtime/Extensions/NetworkDictionaryExtensions.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 
 namespace Unity.Netcode
@@ -7,17 +7,25 @@ namespace Unity.Netcode
     {
         public static Dictionary<TKey, TValue> ToDictionary<TKey, TValue>(this NetworkDictionary<TKey, TValue> self)
             where TKey : unmanaged, IEquatable<TKey>
-            where TValue : unmanaged => ToDictionary(self, keySelector: key => key);
+            where TValue : unmanaged => ToDictionary(self, keySelector: key => key, valueSelector: value => value);
 
         public static Dictionary<TKeyMapped, TValue> ToDictionary<TKey, TValue, TKeyMapped>(this NetworkDictionary<TKey, TValue> self, Func<TKey, TKeyMapped> keySelector)
             where TKey : unmanaged, IEquatable<TKey>
+            where TValue : unmanaged => ToDictionary(self, keySelector, valueSelector: value => value);
+
+        public static Dictionary<TKey, TValueMapped> ToDictionary<TKey, TValue, TValueMapped>(this NetworkDictionary<TKey, TValue> self, Func<TValue, TValueMapped> valueSelector)
+           where TKey : unmanaged, IEquatable<TKey>
+           where TValue : unmanaged => ToDictionary(self, keySelector: key => key, valueSelector);
+
+        public static Dictionary<TKeyMapped, TValueMapped> ToDictionary<TKey, TValue, TKeyMapped, TValueMapped>(this NetworkDictionary<TKey, TValue> self, Func<TKey, TKeyMapped> keySelector, Func<TValue, TValueMapped> valueSelector)
+            where TKey : unmanaged, IEquatable<TKey>
             where TValue : unmanaged
         {
-            var dictionary = new Dictionary<TKeyMapped, TValue>();
+            var dictionary = new Dictionary<TKeyMapped, TValueMapped>();
 
             foreach (var (Key, Value) in self)
             {
-                dictionary.Add(keySelector.Invoke(Key), Value);
+                dictionary.Add(keySelector.Invoke(Key), valueSelector.Invoke(Value));
             }
 
             return dictionary;
@@ -25,15 +33,23 @@ namespace Unity.Netcode
 
         public static void UpdateDictionary<TKey, TValue>(this NetworkDictionary<TKey, TValue> self, IDictionary<TKey, TValue> target)
             where TKey : unmanaged, IEquatable<TKey>
-            where TValue : unmanaged => UpdateDictionary(self, target, keySelector: key => key, keyLookup: key => key);
+            where TValue : unmanaged => UpdateDictionary(self, target, keySelector: key => key, keyLookup: key => key, valueSelector: value => value);
 
         public static void UpdateDictionary<TKey, TValue, TKeyMapped>(this NetworkDictionary<TKey, TValue> self, IDictionary<TKeyMapped, TValue> target, Func<TKey, TKeyMapped> keySelector, Func<TKeyMapped, TKey> keyLookup)
+            where TKey : unmanaged, IEquatable<TKey>
+            where TValue : unmanaged => UpdateDictionary(self, target, keySelector, keyLookup, valueSelector: value => value);
+
+        public static void UpdateDictionary<TKey, TValue, TValueMapped>(this NetworkDictionary<TKey, TValue> self, IDictionary<TKey, TValueMapped> target, Func<TValue, TValueMapped> valueSelector)
+            where TKey : unmanaged, IEquatable<TKey>
+            where TValue : unmanaged => UpdateDictionary(self, target, keySelector: key => key, keyLookup: key => key, valueSelector);
+
+        public static void UpdateDictionary<TKey, TValue, TKeyMapped, TValueMapped>(this NetworkDictionary<TKey, TValue> self, IDictionary<TKeyMapped, TValueMapped> target, Func<TKey, TKeyMapped> keySelector, Func<TKeyMapped, TKey> keyLookup, Func<TValue, TValueMapped> valueSelector)
             where TKey : unmanaged, IEquatable<TKey>
             where TValue : unmanaged
         {
             foreach (var (Key, Value) in self)
             {
-                target[keySelector.Invoke(Key)] = Value;
+                target[keySelector.Invoke(Key)] = valueSelector.Invoke(Value);
             }
 
             foreach (var pair in target)
@@ -52,5 +68,13 @@ namespace Unity.Netcode
         public static NetworkDictionary<TKey, TValue>.OnDictionaryChangedDelegate GetDictionaryUpdator<TKey, TValue, TKeyMapped>(this NetworkDictionary<TKey, TValue> self, IDictionary<TKeyMapped, TValue> target, Func<TKey, TKeyMapped> keySelector, Func<TKeyMapped, TKey> keyLookup)
             where TKey : unmanaged, IEquatable<TKey>
             where TValue : unmanaged => (NetworkDictionaryEvent<TKey, TValue> _) => UpdateDictionary(self, target, keySelector, keyLookup);
+
+        public static NetworkDictionary<TKey, TValue>.OnDictionaryChangedDelegate GetDictionaryUpdator<TKey, TValue, TValueMapped>(this NetworkDictionary<TKey, TValue> self, IDictionary<TKey, TValueMapped> target, Func<TValue, TValueMapped> valueSelector)
+            where TKey : unmanaged, IEquatable<TKey>
+            where TValue : unmanaged => (NetworkDictionaryEvent<TKey, TValue> _) => UpdateDictionary(self, target, valueSelector);
+
+        public static NetworkDictionary<TKey, TValue>.OnDictionaryChangedDelegate GetDictionaryUpdator<TKey, TValue, TKeyMapped, TValueMapped>(this NetworkDictionary<TKey, TValue> self, IDictionary<TKeyMapped, TValueMapped> target, Func<TKey, TKeyMapped> keySelector, Func<TKeyMapped, TKey> keyLookup, Func<TValue, TValueMapped> valueSelector)
+           where TKey : unmanaged, IEquatable<TKey>
+           where TValue : unmanaged => (NetworkDictionaryEvent<TKey, TValue> _) => UpdateDictionary(self, target, keySelector, keyLookup, valueSelector);
     }
 }
